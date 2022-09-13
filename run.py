dc4b9a5b080b30f2f1df09163584fb67eaaa5007
sadafsafi22 <safisadaf96@gmail.com>
2022-09-13 08:38:54 +0000

    Upate Register function Add cheking age and checking for dublicate NID

--- run.py
+++ run.py
@@ -22,9 +22,14 @@ def welcome_msg():
     print('\n \033[1m Welcome to Election Voting System! \033[0m \n')
 
 def has_user():
+    """"
+    This function is the first function,
+    it's allow users to select Login or registred
+    Login is for those user which already regisrated
+    """
 
     while True:
-        has_user = int(input("1. login \n2. Registration \nPlease Select one(1/2):"))
+        has_user = int(input("1. Login \n2. Registration \nPlease Select one(1/2):"))
         if(has_user == 1):
             login()
             break
@@ -37,19 +42,25 @@ def has_user():
 
 
 def login():
-
+    """
+    Request for user to Enter NID and password, 
+    after checking allow users to go voting section
+    """
     print('Welcome to login section \n')
     NID = int(input("Please Enter NID : \n"))
     password = input("please Enter your Password : \n")
     NIDs = users.col_values(2)
+    NIDs.remove('NID')
+    NIDs = [int(num) for num in NIDs]
     passwords = users.col_values(4)
     # print(usernames,password)
 
     while(not((NID in NIDs) and (password in passwords))):
         print(" Sorry NID and password incorrect please re-enter for Validation ")
-        NID = input("Please enter your NID : \n")
+        NID = int(input("Please enter your NID : \n"))
         password = input("Please enter your password : \n")
     
+    print('login secceefully ')
 
 
 def registration():
@@ -60,32 +71,41 @@ def registration():
     user = []
     name = input('Please Enter your Name:\n')
     nid = int(input('Please Enter your National ID (NID):\n'))
+    # checking for dubalicate NID, this system can not Accept dublicate NID
+    saved_nids = users.col_values(2)
+    saved_nids.remove('NID')
+    int_saved_nids = [int(num) for num in saved_nids]
+    # check for dublicate NID
+    while(nid in int_saved_nids):
+        print('Sorry, This NID already registrated, we can not accept dublicate')
+        nid = int(input('Please Enter your own National ID (NID):\n'))
     
-
     dob = int(input('Please Enter Year of your Birth:\n'))
+    check_age(dob)
     password = input('Plase Enter your Password:\n')
-
-    # check if the age is older then 18 
-    age = 2022 - dob
-    if(age <= 18 ):
-        print(f"Sorry, only above 18 years old  can vote.\n Your age is {age}")
-        has_user()
-
     user.append(name)
     user.append(nid)
     user.append(dob)
     user.append(password)
-
     users.append_row(user)
     
     print(f"Registration has been successfuly done.")
-    # print(f'Name: {name} \n NID: {nid}, \n dob: {dob},\n passwrod: {password}')
-
     login()
+
+    
+
+def check_age(dob):
+    """
+    check if the age is older then 18  by using the year of born
+    """
+    year = datetime.date.today().year
+    age = year - dob
+    if(age <= 18 ):
+        print(f"Sorry, only above 18 years old  can vote.\n Your age is {age}")
+        has_user()
 
 
 # welcome_msg()
 # has_user()
-# registration()
-
 registration()
+
